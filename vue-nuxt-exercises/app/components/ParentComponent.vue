461f74a510f54e63e4df3ef405b267e95cdc6cb7
Ruben Collazo <rubencollazonaranjo@gmail.com>
2025-10-14 11:41:17 +0200

    feat(exercises): finixhed exercise 7

---
+++ vue-nuxt-exercises/app/components/ParentComponent.vue
@@ -0,0 +1,9 @@
+import { ChildComponent } from '../../.nuxt/components';
+<template>
+  <ChildComponent @event="notify()"></ChildComponent>
+</template>
+<script setup>
+function notify() {
+  console.log("hola");
+}
+</script>
