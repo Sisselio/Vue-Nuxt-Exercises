d9f74b4d68a4a100c44c0ee54e32111f0848d181
Ruben Collazo <rubencollazonaranjo@gmail.com>
2025-10-15 15:22:07 +0200

    feat(exercises): finixhed exercise 11

--- vue-nuxt-exercises/app/components/TheCounter.vue
+++ vue-nuxt-exercises/app/components/TheCounter.vue
@@ -1,11 +1,22 @@
 <template>
   <TheTitle>Contador:</TheTitle>
-  <p>{{ counter }}</p>
+  <p :class="{ counterFinale: counterFinale }">{{ counter }}</p>
 
   <IncrementButton v-if="counter <= 9" @increment="increment" />
   <DecrementButton v-if="counter >= 1" @decrement="decrement" />
 </template>
 
 <script setup>
+import { computed } from "vue";
+import { useCounter } from "../composables/useCounter";
+
 const { counter, increment, decrement } = useCounter();
+
+const counterFinale = computed(() => counter.value === 10);
 </script>
+
+<style scoped>
+.counterFinale {
+  color: green;
+}
+</style>
