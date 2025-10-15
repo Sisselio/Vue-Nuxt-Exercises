5004c761dfb2a27fdc251e7bb3accf31444f3b76
Ruben Collazo <rubencollazonaranjo@gmail.com>
2025-10-15 17:39:30 +0200

    feat(exercises): installed pinia

--- vue-nuxt-exercises/app/data/mock.ts
+++ vue-nuxt-exercises/app/data/mock.ts
@@ -1,33 +1,35 @@
 // data/mock.ts
 
 export interface Product {
-  id: number
-  image: string
-  title: string
-  description: string
-  price: number
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+  price: number;
 }
 
 export const products: Product[] = [
   {
     id: 1,
-    image: 'https://picsum.photos/id/1011/400/300',
-    title: 'Auriculares Inalámbricos',
-    description: 'Auriculares con cancelación de ruido y batería de larga duración.',
-    price: 59.99
+    image: "https://picsum.photos/id/1011/400/300",
+    title: "Auriculares Inalámbricos",
+    description:
+      "Auriculares con cancelación de ruido y batería de larga duración.",
+    price: 59.99,
   },
   {
     id: 2,
-    image: 'https://picsum.photos/id/1015/400/300',
-    title: 'Cámara Digital',
-    description: 'Cámara profesional con lente de 50mm incluida.',
-    price: 329.99
+    image: "https://picsum.photos/id/1015/400/300",
+    title: "Cámara Digital",
+    description: "Cámara profesional con lente de 50mm incluida.",
+    price: 329.99,
   },
   {
     id: 3,
-    image: 'https://picsum.photos/id/1025/400/300',
-    title: 'Smartwatch Deportivo',
-    description: 'Resistente al agua, monitoreo cardíaco y más de 10 modos de deporte.',
-    price: 89.5
-  }
-]
+    image: "https://picsum.photos/id/1025/400/300",
+    title: "Smartwatch Deportivo",
+    description:
+      "Resistente al agua, monitoreo cardíaco y más de 10 modos de deporte.",
+    price: 89.5,
+  },
+];
